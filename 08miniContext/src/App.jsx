1f7a95c3c56f765e17bff52f671e3439c6f1998b
Akash4056 <akashsaha1313@gmail.com>
2024-01-22 22:02:08 +0530

    theme swithcer project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import UserContextProvider from './context/UserContextProvider'
 import Login from './components/Login'
 import Profile from './components/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <UserContextProvider>
-
       <h1>React</h1>
       <Login/>
       <Profile/>
